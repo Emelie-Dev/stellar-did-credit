415bb60915e1dcb8d5db859998d5d4b1a01de5a8
Emelie-Dev <godfatheremerie@gmail.com>
2026-09-24 13:04:27 +0100

    Fix: Fixed ci issues

--- contracts/revocation-registry/src/lib.rs
+++ contracts/revocation-registry/src/lib.rs
@@ -417,8 +417,9 @@ impl RevocationRegistry {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use soroban_sdk::testutils::Events;
     use proptest::prelude::*;
+    use soroban_sdk::testutils::Events;
+    use soroban_sdk::TryIntoVal;
     use soroban_sdk::{testutils::Address as _, Env};
 
     #[test]
